c4d2956505bb7fe2aad7677ade88fc3ddfd8aa9b
Lokesh Jangir <lk.jangir18@gmail.com>
2022-02-25 10:11:57 +0530

    ddd

--- modules/testp/sample.go
+++ modules/testp/sample.go
@@ -1,16 +1,13 @@
 package test
 
 import (
-	"testing"
 	"fmt"
-	"strings"
-	
-	
+	"testing"
 )
 
 // Basic test to ensure we can successfully generate key pairs (no explicit validation for now)
 func testMsg(t *testing.T) {
-	
-	fmt.Sprintf("Test function"
-	retrun "TESTFunction"
+
+	fmt.Sprintf("Test function")
+
 }
